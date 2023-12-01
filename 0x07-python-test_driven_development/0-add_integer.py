be9074bc5e7704ebfa4f9d57d094181567a4adb2
Arkwings Cyprian Abala <arkwingscyprianabala@gmail.com>
2023-12-01 15:19:14 -0800

    Python Test Driven Environment

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a function that adds integers"""
+
+def add_integer(a, b=98):
+    """Returns the integer addition value of a and b.
+
+    float arguments are typecasted to integers before addition is done
+
+    Raises:
+        TypeError: If either a or b is not integer or float
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return(int(a) + int(b))

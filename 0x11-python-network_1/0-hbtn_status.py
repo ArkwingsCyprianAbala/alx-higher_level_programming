5f9d119d1d1d082df62a6d0cf9294ae03af702f4
Arkwings Cyprian Abala <arkwingscyprianabala@gmail.com>
2024-03-01 16:09:40 -0800

    Python Netwoprking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status."""
 from urllib.request import urlopen
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urlopen(url) as response:
-        html = response.read().decode('utf-8')
-
-for line in html.splitlines():
-    print(f"- {line}")
+        html_by = response.read()
+        html_a = html_by.decode()
+        print("- " + html_a.replace("\n", "\n\t-"))
